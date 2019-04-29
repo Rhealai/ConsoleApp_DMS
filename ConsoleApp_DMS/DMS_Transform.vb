4f7b671171e948efb9afc8df3d59789cd89c25e6
Rhealai <44374177+Rhealai@users.noreply.github.com>
2019-04-30 02:41:05 +0800

    Initial commit

---
+++ ConsoleApp_DMS/DMS_Transform.vb
@@ -0,0 +1,53 @@
+﻿Public Class DMS_Transform
+    Private iDegree As Integer
+    Private iMinute As Integer
+    Private sglSecond As Single
+    Private sglResolution As Single = 10000
+    Private sglPosition As Single
+
+    Public Property Resolution() As Single
+        Get
+            Return sglResolution
+        End Get
+        Set(ByVal value As Single)
+            sglResolution = value
+        End Set
+    End Property
+
+    Public Property Position() As Single
+        Get
+            Return sglPosition
+        End Get
+        Set(ByVal value As Single)
+            sglPosition = value
+        End Set
+    End Property
+
+    Public ReadOnly Property Degree() As Integer
+        Get
+            iDegree = Math.Floor(sglPosition / sglResolution * 360)
+            Return iDegree
+        End Get
+    End Property
+
+    Public ReadOnly Property Minute() As Integer
+        Get
+            iMinute = Math.Floor((sglPosition / sglResolution * 360) * 60 - iDegree * 60)
+            Return iMinute
+        End Get
+    End Property
+
+    Public ReadOnly Property Second() As Single
+        Get
+            sglSecond = (sglPosition / sglResolution * 360) * 60 * 60 - iDegree * 3600 - iMinute * 60
+            If (sglSecond < 0.001) Then
+                sglSecond = 0
+            End If
+            Return sglSecond
+        End Get
+    End Property
+
+
+
+
+End Class
---
+++ ConsoleApp_DMS/Module1.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+
+
+
+    Sub Main()
+
+        aTimer.AutoReset = True
+        aTimer.Interval = 100
+        AddHandler aTimer.Elapsed, AddressOf tick
+        aTimer.Start()
+        'Console.WriteLine(Math.Pow(2, 25))
+        Console.ReadKey()
+    End Sub
+
+    Dim aTimer As New System.Timers.Timer
+    Dim intTmp As Integer = 0
+
+    Private Sub tick(ByVal sender As Object, ByVal e As System.Timers.ElapsedEventArgs)
+
+        Dim dms As New DMS_Transform
+        dms.Resolution = Math.Pow(2, 25)
+        dms.Position = intTmp
+        Console.WriteLine(dms.Degree & Chr(176) & dms.Minute & Chr(34) & dms.Second & Chr(39))
+
+        intTmp += 1
+
+    End Sub
+
+End Module
